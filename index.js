7398fde6043919716b1c78079e57f5d333379c98
Dan Stephenson <ispyhumanfly@gmail.com>
2019-02-13 00:01:36 -0500

    Adding prettier commit hooks, now using yarn, and some other clean up in preparation for release to npm.

--- index.js
+++ index.js
@@ -1,59 +1,70 @@
 "use strict";
 
 /*
- * Avian Fluentd Logger
+ * Fluentd Logger Middleware
  */
 
-var logger = require('fluent-logger');
-var debug = require('debug')('avian-fluentd-logger');
+var logger = require("fluent-logger");
+var debug = require("debug")("avian-fluentd-logger");
 
 /**
  * Create a fluentd logger middleware.
  *
  * @public
- * @param {String|Object} tag
- * @param {String|Object} label
- * @param {Object} options
+ * @param {Object}
  * @return {Function}
  */
-exports = module.exports = function expressFluentLogger(options) {
-
-  options = options || { level: "info", mode: "development", tag: "debug", label: "server", configure: { host: '127.0.0.1', port: 24224, timeout: 3.0 }};
+exports = module.exports = function fluentdLoggerMiddleware(options) {
+  options = options || {
+    level: "info",
+    mode: "development",
+    tag: "debug",
+    label: "server",
+    configure: { host: "127.0.0.1", port: 24224, timeout: 3.0 }
+  };
 
   logger.configure(options.tag, options.configure);
 
-  logger.on('error', debug);
+  logger.on("error", debug);
 
   return function(req, res, next) {
     var start = new Date();
     function emitHandler() {
-      res.removeListener('finish', emitHandler);
-      res.removeListener('close',  emitHandler);
+      res.removeListener("finish", emitHandler);
+      res.removeListener("close", emitHandler);
       var record = {
-        'timestamp':      start.getTime(),
-        'remote-address': req.ip,
-        'method':         req.method,
-        'url':            req.protocol + '://' + req.get('host') + req.originalUrl ? req.originalUrl : req.url,
-        'http-version':   req.httpVersion,
-        'status':         res.statusCode,
-        'content-length': res.get('content-length'),
-        'referrer':       req.get('referrer') || req.get('referer') || '',
-        'response-time':  new Date() - start
+        timestamp: start.getTime(),
+        "remote-address": req.ip,
+        method: req.method,
+        url:
+          req.protocol + "://" + req.get("host") + req.originalUrl
+            ? req.originalUrl
+            : req.url,
+        "http-version": req.httpVersion,
+        status: res.statusCode,
+        "content-length": res.get("content-length"),
+        referrer: req.get("referrer") || req.get("referer") || "",
+        "response-time": new Date() - start
       };
 
-      /** 
+      /**
        * Request Headers
        */
       Object.keys(req.headers)
         .filter(function(key) {
-          return key !== 'host' && key !== 'connection' && key !== 'referrer' && key !== 'referer';
+          return (
+            key !== "host" &&
+            key !== "connection" &&
+            key !== "referrer" &&
+            key !== "referer"
+          );
         })
         .forEach(function(key) {
           key = key.toLowerCase();
           record[key] = req.get(key);
         });
 
-      /** 
+      /**
        * Response Headers
        */
       options.responseHeaders = options.responseHeaders || [];
@@ -66,8 +77,8 @@ exports = module.exports = function expressFluentLogger(options) {
           record[key.toLowerCase()] = res.get(key);
         });
 
-        let componentName = req.path.split("/")
-        componentName = componentName[1]
+      let componentName = req.path.split("/");
+      componentName = componentName[1];
 
       logger.emit(options.label || "server", {
         component: componentName || null,
@@ -77,8 +88,8 @@ exports = module.exports = function expressFluentLogger(options) {
       });
     }
 
-    res.on('finish', emitHandler);
-    res.on('close',  emitHandler);
+    res.on("finish", emitHandler);
+    res.on("close", emitHandler);
     req.logger = logger;
     next();
   };
